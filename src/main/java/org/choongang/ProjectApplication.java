f33bb94e81d0f670709a08e8e6fef5a1a1bed8d3
OasisDefinitelyMaybe <kbh26499@gmail.com>
2023-12-29 14:23:35 +0900

    PC, Mobile 구분

---
+++ src/main/java/org/choongang/ProjectApplication.java
@@ -0,0 +1,10 @@
+package org.choongang;
+
+import org.springframework.boot.SpringApplication;
+
+public class ProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectApplication.class, args);
+    }
+}
