ec836fda93b95337a48cd65714990bb4cc403d9f
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-04 15:21:48 +0900

    관리자 구성

--- src/main/java/org/choongang/commons/BasicConfigAdvice.java
+++ src/main/java/org/choongang/commons/BasicConfigAdvice.java
@@ -2,6 +2,7 @@ package org.choongang.commons;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import lombok.RequiredArgsConstructor;
+import org.choongang.admin.config.controllers.BasicConfig;
 import org.choongang.admin.config.service.ConfigInfoService;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -16,10 +17,12 @@ public class BasicConfigAdvice {
     private final ConfigInfoService infoService;
     
     @ModelAttribute("siteConfig")
-    public Map<String, String> getBasicConfig() {
-        Optional<Map<String, String>> opt = infoService.get("basic", new TypeReference<>() {});
+    public BasicConfig getBasicConfig() {
 
-        Map<String, String> config = opt.orElseGet(() -> new HashMap<>());
+        BasicConfig config = infoService.get("basic", BasicConfig.class).orElseGet(BasicConfig::new);
+
+
+
         return config;
     }
 }
