f32ccb3de29a0bb9c4084658a0f8718f8e25e6be
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-04 14:11:41 +0900

    관리자 구성

--- src/main/java/org/choongang/commons/BasicConfigAdvice.java
+++ src/main/java/org/choongang/commons/BasicConfigAdvice.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
-@ControllerAdvice("org.choongang")
+@ControllerAdvice(value = "org.choongang")
 @RequiredArgsConstructor
 public class BasicConfigAdvice {
     private final ConfigInfoService infoService;
