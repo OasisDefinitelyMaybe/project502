d1c10ab9d68c47645ddaea6c97a495d8b83fb0c0
OasisDefinitelyMaybe <kbh26499@gmail.com>
2024-01-10 18:48:23 +0900

    210110

--- src/main/java/org/choongang/member/entities/Member.java
+++ src/main/java/org/choongang/member/entities/Member.java
@@ -12,29 +12,27 @@ import java.util.List;
 @Data
 @Entity
 public class Member extends Base {
-    @Id
-    @GeneratedValue
+    @Id @GeneratedValue
     private Long seq;
 
-    @Column(length = 65, nullable = false)
+    @Column(length=65, nullable = false)
     private String gid;
 
-    @Column(length = 80, nullable = false, unique = true)
+    @Column(length=80, nullable = false, unique = true)
     private String email;
 
-    @Column(length = 40, nullable = false, unique = true)
+    @Column(length=40, nullable = false, unique = true)
     private String userId;
 
-    @Column(length = 65, nullable = false)
+    @Column(length=65, nullable = false)
     private String password;
 
-    @Column(length = 40, nullable = false)
+    @Column(length=40, nullable = false)
     private String name;
 
     @ToString.Exclude
     @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
     private List<Authorities> authorities = new ArrayList<>();
-
 
     @Transient
     private FileInfo profileImage;
